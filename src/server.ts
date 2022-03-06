d1ccdf2bf013a8e21a4069d2ef9276204d3971e2
Muhammad Charaf <muhammad.charaf@gmail.com>
2022-03-06 20:45:35 +0200

    wip

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,26 @@
 import express from 'express'
 import { AppRouter } from './api/routes'
+import { Sequelize, SequelizeOptions } from 'sequelize-typescript'
 
-(() => {
+async function setupSequelize (): Promise<void> {
+  const sequelize = new Sequelize({
+    database: 'didomi',
+    host: 'localhost',
+    dialect: 'postgres',
+    storage: ':memory:'
+  } as unknown as SequelizeOptions)
+
+  try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully')
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+
+(async () => {
+  await setupSequelize()
+
   const app = express()
   const port = process.env.PORT ?? 8080
 
@@ -15,3 +34,5 @@ import { AppRouter } from './api/routes'
     console.log(`🚀 Server ready at port ${port}`)
   })
 })()
+  .then(() => {})
+  .catch((err) => console.error('Error starting server', err))
